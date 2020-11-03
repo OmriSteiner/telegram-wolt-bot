8cda1151be40532a11b005254e71925efd2a9f4f
Omri Steiner <omri@steiners.co.il>
2020-11-03 19:53:10 +0000

    implement /status and /start

--- src/telegram-wolt-bot/bot.py
+++ src/telegram-wolt-bot/bot.py
@@ -13,6 +13,15 @@ WOLT_URL = f'https://{WOLT_DOMAIN}'
 SEARCH_URL = f'{WOLT_URL}/v1/search'
 RESTAURANT_INFO_URL = f'{WOLT_URL}/v3/venues/slug/'
 
+START_MESSAGE = """Hello!
+In order to wait for a restaurant to become online, type:
+/monitor <restaurant name>
+
+The restaurant's name could be in Hebrew or English!"""
+
+MONITORING_JOBS_KEY = "monitoring"
+MONITOR_INTERVAL = 10 # every 10sec
+
 
 def lookup_restaurant(name):
     result = requests.get(SEARCH_URL, params={"q": name}).json()
@@ -24,6 +33,33 @@ def lookup_restaurant(name):
                 continue
             restaurants.append({'name': language['value'], 'slug': restaurant[u'value']['slug']})
     return restaurants
+
+
+def add_monitoring_job(context, repeating_callback, restaurant_name):
+    job = context.job_queue.run_repeating(repeating_callback, MONITOR_INTERVAL) # every 10sec
+
+    monitoring_jobs = context.bot_data.setdefault(MONITORING_JOBS_KEY, {})
+    monitoring_jobs[job] = restaurant_name
+
+
+def remove_monitoring_job(context):
+    job = context.job
+    if job == None:
+        return
+
+    try:
+        context.bot_data[MONITORING_JOBS_KEY].pop(job)
+    except KeyError:
+        logging.error("Tried removing job but failed.")
+
+    job.schedule_removal()
+
+
+def get_monitored_restaurants(context):
+    try:
+        return list(context.bot_data[MONITORING_JOBS_KEY].values())
+    except KeyError:
+        return []
 
 
 def monitor_restaurant(update, context, index):
@@ -46,17 +82,17 @@ def monitor_restaurant(update, context, index):
         except KeyError:
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text=f'Could not fetch online status. Aborting monitor.')
-            context.job.schedule_removal()
+            remove_monitoring_job(context)
             return
 
         if is_online:
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text=f'Restaurant "{restaurant["name"]}" is online!')
             # Don't run anymore
-            context.job.schedule_removal()
+            remove_monitoring_job(context)
 
 
-    context.job_queue.run_repeating(repeating_job, 10) # every 10sec
+    add_monitoring_job(context, repeating_job, restaurant["name"])
 
 
 def message_callback(update, context):
@@ -92,6 +128,16 @@ def monitor(update, context):
         monitor_restaurant(update, context, 0)
 
 
+def status(update, context):
+    restaurants = get_monitored_restaurants(context)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=str(restaurants))
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=START_MESSAGE)
+
+
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
 
@@ -101,9 +147,13 @@ def main():
     dispatcher = updater.dispatcher
 
     monitor_handler = CommandHandler('monitor', monitor)
+    start_handler = CommandHandler('start', start)
+    status_handler = CommandHandler('status', status)
     message_handler = MessageHandler(Filters.text & (~Filters.command), message_callback)
 
     dispatcher.add_handler(monitor_handler)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(status_handler)
     dispatcher.add_handler(message_handler)
 
     updater.start_polling()
