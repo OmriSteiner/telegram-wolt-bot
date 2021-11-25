8768e003ca48cc5965dfab19dd6445135dc5f40b
Omri Steiner <omri@steiners.co.il>
2021-11-25 20:24:29 +0000

    update bot to work with new search API

--- src/telegram-wolt-bot/bot.py
+++ src/telegram-wolt-bot/bot.py
@@ -10,7 +10,7 @@ import config
 
 WOLT_DOMAIN = 'restaurant-api.wolt.com'
 WOLT_URL = f'https://{WOLT_DOMAIN}'
-SEARCH_URL = f'{WOLT_URL}/v1/search'
+SEARCH_URL = f'{WOLT_URL}/v1/pages/search'
 RESTAURANT_INFO_URL = f'{WOLT_URL}/v3/venues/slug/'
 
 START_MESSAGE = """Hello!
@@ -24,14 +24,24 @@ MONITOR_INTERVAL = 10 # every 10sec
 
 
 def lookup_restaurant(name):
-    result = requests.get(SEARCH_URL, params={"q": name}).json()
+    params = {
+        "q": name,
+        # Since 25.11.2021, Wolt's backend requires your location for searchs.
+        # So heres Dizengoff Center for you...
+        "lat": 32.075409,
+        "lon": 34.775134
+    }
+    result = requests.get(SEARCH_URL, params=params).json()
+    # Result always has a single section.
+    section = result["sections"][0]
+
+    if section["name"] == "no-content":
+        # This means there are no search results
+        return []
 
     restaurants = []
-    for restaurant in result['results']:
-        for language in restaurant[u'value'][u'name']:
-            if language['lang'] != 'en':
-                continue
-            restaurants.append({'name': language['value'], 'slug': restaurant[u'value']['slug']})
+    for restaurant in section["items"]:
+        restaurants.append({"name": restaurant["title"], "slug": restaurant["venue"]["slug"]})
     return restaurants
 
 
